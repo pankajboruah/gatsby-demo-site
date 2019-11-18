03e0202df3dd16263aa476f9e3d55f38451b4ffd
Pankaj Boruah <pankaj.boruah@upgrad.com>
2019-11-18 14:48:29 +0530

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,63 @@
+const _ = require(`lodash`);
+const Promise = require(`bluebird`);
+const path = require(`path`);
+const slash = require(`slash`);
+const queryAll = require(`./src/queries/queryAll.js`)
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+    const { createPage } = boundActionCreators;
+
+
+    return new Promise((resolve, reject) => {
+
+        // Pages
+        graphql(queryAll)
+            .then(result => {
+                if (result.errors) {
+                    console.log(result.errors);
+                    reject(result.errors);
+                }
+
+                const pageTemplate = path.resolve("./src/templates/page.js");
+
+                _.each(result.data.allWordpressPage.edges, edge => {
+                    createPage({
+                        path: `/${edge.node.slug}/`,
+                        component: slash(pageTemplate),
+                        context: {
+                            id: edge.node.id,
+                        },
+                    });
+                });
+            })
+
+            .then(() => {
+                graphql(queryAll)
+                    .then(result => {
+                        if (result.errors) {
+                            console.log(result.errors);
+                            reject(result.errors);
+                        }
+                        const postTemplate = path.resolve("./src/templates/post.js");
+                        const postsTemplate = path.resolve("./src/pages/posts/index.js");
+
+                        createPage({
+                          path: `/posts/`,
+                          component: slash(postsTemplate)
+                        })
+
+                        _.each(result.data.allWordpressPost.edges, edge => {
+                            createPage({
+                                path: `/post/${edge.node.slug}/`,
+                                component: slash(postTemplate),
+                                context: {
+                                    id: edge.node.id,
+                                },
+                            });
+                        });
+                        resolve();
+                    });
+            });
+        // ==== END POSTS ====
+    });
+};
